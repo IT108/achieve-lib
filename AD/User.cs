8a0fe5a96c707be80d05546ef992b163dca8dec1
Dmitry Paramonov <gamiysikdima@gmail.com>
2020-04-26 01:03:47 +0300

    V 0.1.5

---
+++ AD/User.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace achieve_lib.AD
+{
+	class User
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		[JsonProperty("id")]
+		public string Id { get; set; }
+		
+		[BsonElement("name")]
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[BsonElement("surname")]
+		[JsonProperty("surname")]
+		public string Surname { get; set; }
+
+		[BsonElement("group")]
+		[JsonProperty("group")]
+		public string Group { get; set; }
+
+		[BsonElement("adUser")]
+		[JsonProperty("adUser")]
+		public ADUser AdUser { get; set; }
+
+		[BsonElement("interests")]
+		[JsonProperty("interests")]
+		public List<string> Interests { get; set; }
+	}
+}
