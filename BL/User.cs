6824e1a6e989abfb83a11969ff7b7644de2932f7
Dmitry Paramonov <gamiysikdima@gmail.com>
2020-05-03 21:31:28 +0300

    some user fields

--- BL/User.cs
+++ BL/User.cs
@@ -19,28 +19,52 @@ namespace achieve_lib.BL
 		[JsonProperty("identityID")]
 		public string IdentityId { get; set; }
 
+		[BsonElement("organizationID")]
+		[JsonProperty("organizationID")]
+		public string OrganizationId { get; set; }
+		
+		[BsonElement("grade")]
+		[JsonProperty("grade")]
+		public string Grade { get; set; } = "0";
+
+		[BsonElement("gradeLiteral")]
+		[JsonProperty("gradeLiteral")]
+		public string GradeLiteral { get; set; } = "null";
+
+		[BsonElement("room")]
+		[JsonProperty("room")]
+		public string Room { get; set; } = "null";
+
+		[BsonElement("email")]
+		[JsonProperty("email")]
+		public string Email { get; set; } = "null";
+
 		[BsonElement("name")]
 		[JsonProperty("name")]
-		public string Name { get; set; }
+		public string Name { get; set; } = "None";
 
 		[BsonElement("surname")]
 		[JsonProperty("surname")]
-		public string Surname { get; set; }
+		public string Surname { get; set; } = "None";
 
 		[BsonElement("group")]
 		[JsonProperty("group")]
-		public string Group { get; set; }
+		public string Group { get; set; } = "None";
 
 		[BsonElement("adUser")]
 		[JsonProperty("adUser")]
-		public ADUser AdUser { get; set; }
+		public ADUser AdUser { get; set; } = new ADUser();
 
 		[BsonElement("interests")]
 		[JsonProperty("interests")]
-		public List<string> Interests { get; set; }
+		public List<string> Interests { get; set; } = new List<string>();
+
+		[BsonElement("description")]
+		[JsonProperty("description")]
+		public string Description { get; set; } = "";
 
 		[BsonElement("activities")]
 		[JsonProperty("activities")]
-		public List<string> Activities { get; set; }
+		public List<string> Projects { get; set; } = new List<string>();
 	}
 }
